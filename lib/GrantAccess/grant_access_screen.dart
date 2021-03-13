14074eb93068e86a49b55f8499a3dc28adcbfaa3
Ameed Sayeh <ameed.iyad.sayeh@gmail.com>
2021-03-13 11:59:18 +0200

    Added http dependency

--- lib/GrantAccess/grant_access_screen.dart
+++ lib/GrantAccess/grant_access_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:loading_animations/loading_animations.dart';
 
 class GrantAccessScreen extends StatelessWidget {
   @override
