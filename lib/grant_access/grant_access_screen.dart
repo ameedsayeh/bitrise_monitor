3a30e56b731e01792507c8782bcd12acca5931ee
Ameed Sayeh <ameed.iyad.sayeh@gmail.com>
2021-03-20 21:26:35 +0200

    loading state

--- lib/grant_access/grant_access_screen.dart
+++ lib/grant_access/grant_access_screen.dart
@@ -3,7 +3,7 @@ import 'package:flutter_svg/svg.dart';
 import 'package:bitrise_monitor/Services/bitrise_client.dart';
 import 'widgets/enter_token_text.dart';
 import 'widgets/grant_access_button.dart';
-import 'widgets/how_to_text.dart';
+import 'widgets/how_to_get_token_button.dart';
 import 'widgets/token_textfield.dart';
 
 class GrantAccessScreen extends StatefulWidget {
@@ -14,6 +14,8 @@ class GrantAccessScreen extends StatefulWidget {
 class _GrantAccessScreenState extends State<GrantAccessScreen> {
   final tokenTextController = TextEditingController();
 
+  var isLoading = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,33 +25,60 @@ class _GrantAccessScreenState extends State<GrantAccessScreen> {
   }
 
   Widget buildPage(BuildContext context) {
-    return SingleChildScrollView(
-      physics: AlwaysScrollableScrollPhysics(),
-      child: SizedBox(
-        height: MediaQuery.of(context).size.height,
-        child: SafeArea(
-          child: Padding(
-            padding:
-                const EdgeInsets.symmetric(horizontal: 24.0, vertical: 32.0),
-            child: Column(
-              children: [
-                SizedBox(
-                    height: 150,
-                    child: SvgPicture.asset("lib/assets/images/lock.svg")),
-                EnterTokenText(),
-                TokenTextField(textController: this.tokenTextController),
-                HowToText(),
-                GrantAccessButton(
-                  onClick: () {
-                    BitriseClient.instance
-                        .checkAuthorization(this.tokenTextController.text);
-                  },
+    return Stack(
+      children: [
+        SingleChildScrollView(
+          physics: AlwaysScrollableScrollPhysics(),
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height,
+            child: SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 24.0, vertical: 32.0),
+                child: Column(
+                  children: [
+                    SizedBox(
+                        height: 150,
+                        child: SvgPicture.asset("lib/assets/images/lock.svg")),
+                    EnterTokenText(),
+                    TokenTextField(textController: this.tokenTextController),
+                    HowToGetTokenButton(),
+                    GrantAccessButton(
+                      onClick: () {
+                        setState(() {
+                          this.isLoading = true;
+                        });
+
+                        BitriseClient.instance
+                            .checkAuthorization(this.tokenTextController.text)
+                            .then((value) {
+                          setState(() {
+                            this.isLoading = false;
+                          });
+                        });
+                      },
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
           ),
         ),
-      ),
+        this.isLoading
+            ? Container(
+                color: Color(0x77000000),
+                child: Center(
+                  child: SizedBox(
+                    width: 40,
+                    height: 40,
+                    child: CircularProgressIndicator(
+                      valueColor: AlwaysStoppedAnimation(Color(0xFF381A4B)),
+                    ),
+                  ),
+                ),
+              )
+            : SizedBox(),
+      ],
     );
   }
 }
